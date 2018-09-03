3aa21a343c05c4c705d17d05986d1e1694cc8b49
Doug Callaway <DCAL12@users.noreply.github.com>
2018-09-03 17:17:55 +1200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/ash
 set -e
 
-if [ -f config/application.tmp ]; then
-   envsubst < config/application.tmp > application.yml
+if [ -f config/application.yaml ]; then
+   ln -s config/application.yaml application.yml
 fi
 
 exec "$@"
