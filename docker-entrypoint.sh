bc30a769da5db38fef419674fe0b263c1c6c6459
Doug Callaway <DCAL12@users.noreply.github.com>
2018-09-19 11:05:34 +1200

    Update docker-entrypoint.sh
    make the shinyproxy configuration available to the application

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,13 +2,20 @@
 set -e
 
 # standardise yaml file extension
-if [ -f application.yaml ]; then
-   mv application.yaml application.yml
+if [ -f application.yml ]; then
+   mv application.yml application.yaml
 fi
 
 # substitute ${ENV_VARIABLE} with container environment variables
 if [ -f application.tmp ]; then
-   envsubst < application.tmp > application.yml
+   envsubst < application.tmp > application.yaml
 fi
+
+# make the shinyproxy configuration available to the application
+if [ -f application.yaml ]; then
+   ln -s application.yaml ${INSTALL_DIR}/application.yml
+fi
+
+cd ${INSTALL_DIR}
 
 exec "$@"
