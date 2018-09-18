1135e92bd2634d5eb5cac371542b705495ad7a7b
Doug Callaway <DCAL12@users.noreply.github.com>
2018-09-19 10:33:59 +1200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/ash
 set -e
+
+# standardise yaml file extension
+if [ -f application.yaml ]; then
+   mv application.yaml application.yml
+fi
+
+# substitute ${ENV_VARIABLE} with container environment variables
 if [ -f application.tmp ]; then
    envsubst < application.tmp > application.yml
 fi
+
 exec "$@"
