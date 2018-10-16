e88e56756e5f3ba9a872d0b59b54e3f1931dd83f
Doug Callaway <DCAL12@users.noreply.github.com>
2018-10-16 15:55:07 +1300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,7 +12,7 @@ if [ -f application.yml ]; then
    mv application.yml application.yaml
 fi
 
-# substitute ${ENV_VARIABLE} with container environment variables
+# substitute ${ENV_VARIABLE}s with container environment variables
 if [ -f application.tmp ]; then
    echo "  - found application.tmp; interpolating to application.yaml"
    envsubst < application.tmp > application.yaml
@@ -20,12 +20,12 @@ fi
 
 # make the shinyproxy configuration available to the application
 if [ -f 'application.yaml' ]; then
-   echo "  - creating symbolic link to ${INSTALL_DIR}/application.yml"
    ln -s ${PWD}/application.yaml ${INSTALL_DIR}/application.yml
-   cat ${INSTALL_DIR}/application.yml
+   echo "  - created symbolic link from ${PWD}/application.yaml to ${INSTALL_DIR}/application.yml"
+   
    echo "Completed ShinyProxy configuration"
 else
-   echo "WARNING: no 'application.yml', 'application.yaml', or 'application.tmp' was found; defaulting to demo configuration"
+   echo "WARNING: no 'application.yml', 'application.yaml', or 'application.tmp' was found: defaulting to demo configuration"
 fi
 
 cd ${INSTALL_DIR}
