fd9c7e391e5311eefe05b19b4cde944817af9579
Doug Callaway <DCAL12@users.noreply.github.com>
2018-10-16 14:03:19 +1300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,27 +1,30 @@
 #!/bin/ash
 set -e
 
+echo "Initialising ShinyProxy configuration..."
+
 cd ${CONFIG_DIR}
-echo "checking $PWD for config file"
+echo "  - checking $PWD for config file"
 
 # standardise yaml file extension
 if [ -f application.yml ]; then
-   echo "found application.yml; renaming to application.yaml"
+   echo "  - found application.yml; renaming to application.yaml"
    mv application.yml application.yaml
 fi
 
 # substitute ${ENV_VARIABLE} with container environment variables
 if [ -f application.tmp ]; then
-   echo "found application.tmp; interpolating to application.yaml"
+   echo "  - found application.tmp; interpolating to application.yaml"
    envsubst < application.tmp > application.yaml
 fi
 
 # make the shinyproxy configuration available to the application
 if [ -f application.yaml ]; then
-   echo "creating symbolic link to ${INSTALL_DIR}/application.yml"
+   echo "  - creating symbolic link to ${INSTALL_DIR}/application.yml"
    ln -s application.yaml ${INSTALL_DIR}/application.yml
 fi
 
 cd ${INSTALL_DIR}
+echo "Completed ShinyProxy configuration"
 
 exec "$@"
